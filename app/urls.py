a61001f0c3f089254f7fde69ed70075707522ef1
Avirat201189221 <avirat0603@gmail.com>
2023-03-19 22:52:33 +0530

    ,

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('pixel',views.pixel,name='register'),
     path('wtb',views.wtb,name='whatthebuzz'),
-    path('success',views.sucess,name='success'),
     # path('movie',views.mn,name='movie'),
     path('speaker',views.speaker,name='speaker'),
     path('code',views.code,name='codeoclock'),
