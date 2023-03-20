48f818b851569fe586c41d6a4b0c2b38cb444dc6
Avirat201189221 <avirat0603@gmail.com>
2023-03-20 15:55:28 +0530

    small fix in INSTALLED_APPS

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import-export',
+    'import_export',
 ]
 
 MIDDLEWARE = [
